549f90cd37e746a89b6a9dcc5a0007978b0a13a9
Lucas Nogueira <lucasbitnog@hotmail.com>
2018-09-15 19:35:27 -0300

    Adicionando arquivos de classes que serão usadas nas entidades.

--- src/main/java/com/pontointeligente/utils/SenhaUtils.java
+++ src/main/java/com/pontointeligente/utils/SenhaUtils.java
@@ -1,17 +1,23 @@
 package com.pontointeligente.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SenhaUtils {
+
+	private static final Logger log = LoggerFactory.getLogger(SenhaUtils.class);
 
 	public static String gerarBcrypt(String senha) {
 		if (StringUtils.isEmpty(senha)) {
 			return senha;
 		}
+		log.info("Gerando hash com BCrypt para senha: " + senha);
 		return new BCryptPasswordEncoder().encode(senha);
 	}
 
 	public static boolean isSenhaValida(String senha, String senhaEncoded) {
+		log.info("Validando senha:" + senha + " | " + senhaEncoded);
 		return new BCryptPasswordEncoder().matches(senha, senhaEncoded);
 	}
 }
